af964ba64c48e4849a41a92bdf4955673372bd06
Adarsh Kumar Mishra <143282605+adarshm858@users.noreply.github.com>
2026-04-02 21:55:09 +0530

    Update Book.java

--- JAVA/test3/Book.java
+++ JAVA/test3/Book.java
@@ -1,13 +1,10 @@
 package test3;
-
-// The class name must be uppercase 'Book' to match your other files
 public class Book {
     private String bookId;
     private String title;
     private String author;
     private boolean isAvailable;
-
-    // Constructor
+    
     public Book(String bookId, String title, String author) {
         this.bookId = bookId;
         this.title = title;
@@ -15,14 +12,14 @@ public class Book {
         this.isAvailable = true;
     }
 
-    // Getters and Setters (Encapsulation)
+
     public String getBookId() { return bookId; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public boolean isAvailable() { return isAvailable; }
     public void setAvailable(boolean available) { isAvailable = available; }
 
-    // Overriding toString for clean printing
+   
     @Override
     public String toString() {
         return String.format("[%s] %s by %s - %s",
